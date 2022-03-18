0d1d45d73bb0762796db8021f118e806f415e255
Phil <philipmathew101@gmail.com>
2022-03-17 21:45:54 -0400

    Added back dual-monitor support

--- config/.config/bspwm/regen-theme.sh
+++ config/.config/bspwm/regen-theme.sh
@@ -5,10 +5,10 @@ notify-send -u LOW "Regenerating themes..."
 # Oomox (gtk) theme
 oomox-materia-cli -t ~/.themes -o wal-oomox ~/.cache/wal/colors-oomox
 
-# Spicetify
-spicetify update
-spicetify apply 
-killall spotify 
+# # Spicetify
+# spicetify update
+# spicetify apply 
+# killall spotify 
 
 # Dunst (it's weird man)
 sh ~/.config/dunst/launch.sh
